a5f688916a12b868f78ab34ecf76f6c235085c6f
stephya101 <stephy101.sa@gmail.com>
2022-02-02 17:53:07 -0500

    Created my first flask app

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ This file creates your application.
 from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return 'My home page'
-
 '''
 # Routing for your application.
 # Put your routes below this comment
 '''
+@app.route("/")
+def home():
+    return 'My home page'
+
+@app.route('/about')
+def about():
+ return render_template('about.html')
 
 
 @app.errorhandler(404)
